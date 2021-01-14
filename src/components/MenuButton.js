350e4a50a675ffcad766d6347724e72ad66a41d2
Dariusz Nowacki <dariusz.nowacki@sky.uk>
2021-01-14 15:07:30 +0000

    Update Components

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,9 +1,8 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import '../global/_variables.css';
 const MenuButton = props => {
     return (
-<Link style={{ marginRight: "5px", color:"var(--colors-white-100)", fontFamily:"var(----typography-h-1-font-family)"}} to={props.to} className="btn btn-secondary">
+      <Link style={{ marginRight: "5px", color:"var(--colors-white-100)", fontSize:"var(----typography-h-5-font-size)"}} to={props.to} className="btn btn-secondary">
         {props.name}
       </Link>
   );
